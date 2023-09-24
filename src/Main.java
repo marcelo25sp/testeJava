d6f76f6bceca1b6b6b15ed626624f9b080548dcf
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-09-24 14:34:14 -0300

    Primeiro programa teste em Java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Bem vindo ao mundo Java!");
+		// dando os primeiros passos para ser um programador Java
+		
+		
+
+	}
+
+}
